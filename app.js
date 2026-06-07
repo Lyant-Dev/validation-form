2e963efcca162e30212adb71abb3acd6f7f3f84e
rahmatbrylyant975@gmail.com <rahmatbrylyant975@gmail.com>
2026-06-07 22:40:46 +0700

    feat: setup DOM element references in app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+const form = document.querySelector(".form-data");
+const nameInput = document.querySelector("#name");
+const numberInput = document.querySelector("#number");
+const emailInput = document.querySelector("#email");
+const messageInput = document.querySelector("#message");
+const submitBtn = document.querySelector(".submit-btn");
+
