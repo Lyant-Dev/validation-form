7c000b791e9332ac96b95404da8d295a137de6cf
rahmatbrylyant975@gmail.com <rahmatbrylyant975@gmail.com>
2026-06-08 23:20:49 +0700

    starting validateName

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ const emailInput = document.querySelector("#email");
 const messageInput = document.querySelector("#message");
 const submitBtn = document.querySelector(".submit-btn");
 
+function validateName() {
+  const value = nameInput.value.trim();
+  if (value === "") {
+    console.log("Nama wajib diisi!");
+  } else if (value.length < 2) {
+    console.log("Tidak boleh kurang daru 2 karakter");
+  }
+}
+nameInput.addEventListener("blur", validateName);
